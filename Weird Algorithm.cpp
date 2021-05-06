cab9be705f18e0550dfc4b14f92a67d664204ced
Ojasvi Chauhan <ojasvichauhan2304@gmail.com>
2021-05-06 21:04:01 +0530

    Update Weird Algorithm.cpp

--- Weird Algorithm.cpp
+++ Weird Algorithm.cpp
@@ -13,53 +13,53 @@ int mod = 1000000007;
 
 void solve()
 {
-	int n;
-	cin >> n;
-	int A[n - 1];
-	for (int i = 0; i < n - 1; i++) {
-		cin >> A[i];
-	}
+    int n;
+    cin >> n;
+    int A[n - 1];
+    for (int i = 0; i < n - 1; i++) {
+        cin >> A[i];
+    }
 
-	int i = 0;
-	while (i < n - 1) {
-		int idx = A[i] - 1;
-		if (idx < n - 1 and A[i] != i + 1 and A[idx] != idx + 1) {
-			swap(A[i], A[idx]);
-		}
-		else i++;
-	}
-	//print(A);
-	for (int i = 0; i < n; i++) {
-		if (A[i] != i + 1) {
-			cout << i + 1 << endl;
-			return;
-		}
-	}
+    int i = 0;
+    while (i < n - 1) {
+        int idx = A[i] - 1;
+        if (idx < n - 1 and A[i] != i + 1 and A[idx] != idx + 1) {
+            swap(A[i], A[idx]);
+        }
+        else i++;
+    }
+    //print(A);
+    for (int i = 0; i < n; i++) {
+        if (A[i] != i + 1) {
+            cout << i + 1 << endl;
+            return;
+        }
+    }
 }
 
 int32_t main() {
-	fast;
+    fast;
 
 #ifndef ONLINE_JUDGE
-	auto begin = std::chrono::high_resolution_clock::now();
+    auto begin = std::chrono::high_resolution_clock::now();
 #endif
 
 #ifndef ONLINE_JUDGE
-	freopen("input.txt", "r", stdin);
-	freopen("output.txt", "w", stdout);
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
 #endif
 
-	// int t;
-	// cin >> t;
-	// while (t--)
-	solve();
+    // int t;
+    // cin >> t;
+    // while (t--)
+    solve();
 
 
 #ifndef ONLINE_JUDGE
-	auto end = std::chrono::high_resolution_clock::now();
-	cerr << setprecision(7) << fixed;
-	cerr << "Execution time: " << std::chrono::duration_cast<std::chrono::duration<double>>(end - begin).count() << " seconds" << endl;
+    auto end = std::chrono::high_resolution_clock::now();
+    cerr << setprecision(7) << fixed;
+    cerr << "Execution time: " << std::chrono::duration_cast<std::chrono::duration<double>>(end - begin).count() << " seconds" << endl;
 #endif
 
-	return 0;
+    return 0;
 }
