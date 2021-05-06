0cc9435ec8e0cef40522b6de8f0fb563105f2a7b
Ojasvi Chauhan <ojasvichauhan2304@gmail.com>
2021-05-06 21:03:20 +0530

    Update Weird Algorithm.cpp

--- Weird Algorithm.cpp
+++ Weird Algorithm.cpp
@@ -9,54 +9,57 @@ using namespace std;
 #define fast ios_base::sync_with_stdio(0);cin.tie(NULL);cout.tie(NULL);
 #define print(v) for(auto x:v){cout<<x<<" ";}cout<<endl;
 int mod = 1000000007;
- 
-void helper(int n, vector<int> &ans) {
-	if (n == 1) {
-		return;
-	}
-	else if (n % 2 == 0) {
-		ans.push_back(n / 2);
-		helper(n / 2, ans);
-	}
-	else if (n % 2 != 0) {
-		ans.push_back(3 * n + 1);
-		helper(3 * n + 1, ans);
-	}
-}
- 
+
+
 void solve()
 {
 	int n;
 	cin >> n;
-	vector<int> ans;
-	ans.push_back(n);
-	helper(n, ans);
-	print(ans);
+	int A[n - 1];
+	for (int i = 0; i < n - 1; i++) {
+		cin >> A[i];
+	}
+
+	int i = 0;
+	while (i < n - 1) {
+		int idx = A[i] - 1;
+		if (idx < n - 1 and A[i] != i + 1 and A[idx] != idx + 1) {
+			swap(A[i], A[idx]);
+		}
+		else i++;
+	}
+	//print(A);
+	for (int i = 0; i < n; i++) {
+		if (A[i] != i + 1) {
+			cout << i + 1 << endl;
+			return;
+		}
+	}
 }
- 
+
 int32_t main() {
 	fast;
- 
+
 #ifndef ONLINE_JUDGE
 	auto begin = std::chrono::high_resolution_clock::now();
 #endif
- 
+
 #ifndef ONLINE_JUDGE
 	freopen("input.txt", "r", stdin);
 	freopen("output.txt", "w", stdout);
 #endif
- 
+
 	// int t;
 	// cin >> t;
 	// while (t--)
 	solve();
- 
- 
+
+
 #ifndef ONLINE_JUDGE
 	auto end = std::chrono::high_resolution_clock::now();
 	cerr << setprecision(7) << fixed;
 	cerr << "Execution time: " << std::chrono::duration_cast<std::chrono::duration<double>>(end - begin).count() << " seconds" << endl;
 #endif
- 
+
 	return 0;
 }
