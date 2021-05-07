2fe804f79a521c0642c47cd091824c1648baeac5
Ojasvi Chauhan <ojasvichauhan2304@gmail.com>
2021-05-07 12:34:38 +0530

    Create Trailing Zeros.cpp

---
+++ Trailing Zeros.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fl(i,a,b) for(int i=a;i<b;i++)
+#define pb push_back
+#define endl '\n'
+#define int long long
+#define ff first
+#define ss second
+#define fast ios_base::sync_with_stdio(0);cin.tie(NULL);cout.tie(NULL);
+#define print(v) for(auto x:v){cout<<x<<" ";}cout<<endl;
+int mod = 1000000007;
+
+// ------------------------------------------------------------------------------------------------
+
+void solve()
+{
+    int n;
+    cin >> n;
+
+    int cnt = 0;
+    for (int i = 5; n / i >= 1; i *= 5) {
+        cnt += n / i;
+    }
+
+    cout << cnt << endl;
+}
+
+// ------------------------------------------------------------------------------------------------
+
+int32_t main() {
+    fast;
+
+#ifndef ONLINE_JUDGE
+    auto begin = std::chrono::high_resolution_clock::now();
+#endif
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    // int t;
+    // cin >> t;
+    // while (t--)
+    solve();
+
+
+#ifndef ONLINE_JUDGE
+    auto end = std::chrono::high_resolution_clock::now();
+    cerr << setprecision(7) << fixed;
+    cerr << "Execution time: " << std::chrono::duration_cast<std::chrono::duration<double>>(end - begin).count() << " seconds" << endl;
+#endif
+
+    return 0;
+}
