d7f10cde8349bfc072dce1f51b8947b44e672852
Ojasvi Chauhan <ojasvichauhan2304@gmail.com>
2021-05-08 21:08:47 +0530

    Create Coin Piles.cpp

---
+++ Coin Piles.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fl(i,a,b) for(int i=a;i<b;i++)
+#define pb push_back
+#define endl '\n'
+#define int long long
+#define ff first
+#define ss second
+#define fast ios_base::sync_with_stdio(0);cin.tie(NULL);cout.tie(NULL);
+#define print(v) for(auto x:v){cout<<x<<" ";}cout<<endl;
+int mod = 1000000007;
+
+// ------------------------------------------------------------------------------------------------
+
+void solve()
+{
+    int a, b;
+    cin >> a >> b;
+
+    if (a < b) swap(a, b);
+    if (a > 2 * b) {
+        cout << "NO\n";
+    }
+    else {
+        a %= 3, b %= 3;
+        if (a < b) swap(a, b);
+        if ((a == 2 && b == 1) || (a == b && b == 0)) {
+            cout << "YES\n";
+        }
+        else {
+            cout << "NO\n";
+        }
+    }
+}
+
+// ------------------------------------------------------------------------------------------------
+
+int32_t main() {
+    fast;
+
+#ifndef ONLINE_JUDGE
+    auto begin = std::chrono::high_resolution_clock::now();
+#endif
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    int t;
+    cin >> t;
+    while (t--)
+        solve();
+
+
+#ifndef ONLINE_JUDGE
+    auto end = std::chrono::high_resolution_clock::now();
+    cerr << setprecision(7) << fixed;
+    cerr << "Execution time: " << std::chrono::duration_cast<std::chrono::duration<double>>(end - begin).count() << " seconds" << endl;
+#endif
+
+    return 0;
+}
